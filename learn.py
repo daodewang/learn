913f540107ecc7ff7dbf0052b3b9a8b679ac2739
toad_lenovo <daodewang@gmail.com>
2019-10-23 16:28:47 +0800

    add html

--- learn.py
+++ learn.py
@@ -2,18 +2,54 @@
 pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
 pairs.sort(key=lambda pair: pair[1])
 print(pairs)
-
-
+__import__('os').system('dir')
 def f(a):
     a.append(1)
     print(a)
 
-
 f(pairs)
 print(pairs)
 
+b=7
+
+def f(a):
+    print(a+b)
+
+f(2)
+print(b)
 
 
+# IO-input
+name = 'Liming'
+print('my name is %s.' % name)
+print(f'my name is {name!s}.')
+age = eval(input('input your age\n'))
+print(age)
+
+# IO-file
+f = open('1.jpg', 'rb')
+#s = f.read(4)
+for s in f:
+    print(s)
+    print(f.tell())
+
+f.close()
+
+
+
+# IO-json
+import json
+f = open('testj.json', 'r')
+#l = [123,'123',[123,'123']]
+s = json.load(f)
+print(s)
+print(type(s))
+#f.write(s)
+f.close()
+
+
+
+# try
 def bool_return():
     try:
         a=1
@@ -39,14 +75,8 @@ except ArithmeticError:
     print('算术错误，您不能输入0')
 else:
     print('没有出现异常')
-b=7
 
-def f(a):
-    print(a+b)
-
-f(2)
-print(b)
-
+# class
 class MyClass:
     i = 12345
 
@@ -54,17 +84,57 @@ class MyClass:
         return 'hello world'
 
 x = MyClass()
-print(id(MyClass.i))
-print(id(x.i))
-print(id(MyClass._MyClass__f))
-x.f=100
-print(id(x.f))
-print(type(x))
+#x._f()
+MyClass.bb=12345
+x.aa = '123'
+print(x._MyClass__f())
+
+
+
+def fn(self, name='world'):
+    print('Hello, %s.' % name)
+
+dct = {'hello':fn, 'i':10}
+Hello = type('Hello', (object,), dct)
+
+#He = type('Hello')
+print(type(Hello))
+h = Hello()
+print(h.i)
+''''''
+MyClass.ff = fn
+x.ff()
+
+
+
+class ListMetaclass(type):
+  def __new__(cls, name, bases, attrs):
+    attrs['add'] = lambda self, value: self.append(value)
+    return type.__new__(cls, name, bases, attrs)
+
+class MyList(list, metaclass=ListMetaclass):
+    pass
+
+
+# generator
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield(3)
+    print('step 3')
+    yield(5)
 '''
 
-# IO
-name = 'Liming'
-print('my name is %s.' % name)
-print(f'my name is {name!s}.')
-age = eval(input('input your age\n'))
-print(age)
+import re
+
+input_str = 'I LIKE Python3 and Pyth i like Python2.7'
+pattern = r'(P|p)yth(on)'
+match_python = re.findall(pattern, input_str)
+print(match_python)
+
+s = input_str
+
+match = re.search(pattern, s)
+if match:
+    print(match)
--- modA.py
+++
@@ -1,6 +0,0 @@
-import sys
-import modB
-print(modB.intoo(123))
-print("I'm A")
-print(__name__)
-print(sys.path)
